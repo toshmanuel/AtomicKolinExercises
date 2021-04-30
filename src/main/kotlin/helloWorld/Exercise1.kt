c96ca77e93f6ebb652fe4a1ba87c83ef2f4494e0
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-04-30 14:06:01 +0100

    Kotlin Exercises

---
+++ src/main/kotlin/helloWorld/Exercise1.kt
@@ -0,0 +1,5 @@
+package helloWorld
+
+fun main(args: Array<String>) {
+    print("Hello, Kotlin!")
+}
---
+++ src/main/kotlin/helloWorld/Exercise3.kt
@@ -0,0 +1,6 @@
+package helloWorld
+
+fun main(args: Array<String>) {
+//    print("Hello\n")
+    print("Kotlin!")
+}
