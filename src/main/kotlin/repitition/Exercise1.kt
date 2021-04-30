9c43de539b2f0647f6fa5f73644db60957407bc9
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-04-30 20:42:28 +0100

    Repition with while loop Exercise 1

---
+++ src/main/kotlin/repitition/Exercise1.kt
@@ -0,0 +1,14 @@
+package repitition
+
+fun displayContent(contents: String) {
+    var counter = 0
+    while(counter < contents.length){
+
+        println(contents[counter])
+        counter++
+    }
+}
+
+fun main(args: Array<String>) {
+    displayContent("This is my Laptop")
+}
