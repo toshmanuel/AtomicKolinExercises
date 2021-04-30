863550d50a6f45500276915b42f2078e76df346a
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-04-30 22:48:41 +0100

    Looping Exercise 2

---
+++ src/main/kotlin/lopping/Exercise2.kt
@@ -0,0 +1,15 @@
+package lopping
+
+
+fun sumOfFactorial(value: Int): Long{
+    var total = 0L
+    for (i in 1 .. value){
+        total += factorial(i)
+    }
+    return total
+}
+
+
+fun main(args: Array<String>) {
+    println(sumOfFactorial(10))
+}
