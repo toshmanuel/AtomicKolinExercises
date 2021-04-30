e82e4f98cb848125f5a5fcf0b56ea974f996902d
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-04-30 21:48:39 +0100

    Repition with while loop Exercise 3

--- src/main/kotlin/repetition/Exercise3.kt
+++ src/main/kotlin/repetition/Exercise3.kt
@@ -1,16 +1,18 @@
 package repetition
 
-fun repetitiveSum(number : Int){
+fun repetitiveEvenSum(number : Int){
     if(number <= 0) return
     var total = 0
     var counter = 1
     while (counter <= number){
-        total += counter
+        if(counter % 2 == 0){
+            total += counter
+        }
         counter++
     }
     println(total)
 }
 
 fun main(args: Array<String>) {
-    repetitiveSum(12)
+    repetitiveEvenSum(12)
 }
