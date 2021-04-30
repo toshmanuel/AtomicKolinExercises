2713d2a3e336e050b07be49cbf478fa010823bed
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-04-30 16:33:00 +0100

    Functions Exercise 2

---
+++ src/main/kotlin/functions/Exercise2.kt
@@ -0,0 +1,10 @@
+package functions
+
+fun sumDouble(x: Double, y:Double, z:Double): Double {
+    return x + y + z
+}
+
+fun main(args: Array<String>) {
+    val result = sumDouble(1.1, 2.4, 2.5)
+    println(result)
+}
