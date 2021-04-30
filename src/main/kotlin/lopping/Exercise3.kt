f889f65bce1c5efd40f823c7502a8c3a474ba43f
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-04-30 22:56:06 +0100

    Looping Exercise 3

---
+++ src/main/kotlin/lopping/Exercise3.kt
@@ -0,0 +1,13 @@
+package lopping
+
+fun sumEven(value: Int): Int{
+    var total = 0
+    for (i in 1..value){
+        if (i % 2 ==0) total += i
+    }
+    return total
+}
+
+fun main(args: Array<String>) {
+    println(sumEven(10))
+}
