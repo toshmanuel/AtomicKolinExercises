d4b59311a6658458c152b33c5fe89bd6ec2630de
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-04-30 15:52:29 +0100

    Data Types Exercise 2

--- src/main/kotlin/dataTypes/Exercise1.kt
+++ src/main/kotlin/dataTypes/Exercise1.kt
@@ -1,11 +1,11 @@
-package DataTypes
+package dataTypes
 
 fun main(args: Array<String>) {
 //    var answer: Int = 42
 //    answer = "You"
 
-    var answer = 42
-    answer = "You"
+//    var answer = 42
+//    answer = "You"
 
 
 
---
+++ src/main/kotlin/dataTypes/Exercise2.kt
@@ -0,0 +1,13 @@
+package dataTypes
+
+fun main(args: Array<String>) {
+    val int: Int = 10
+    val double: Double = 1.1
+    val boolean: Boolean = false
+    val string: String = "abc"
+    val char: Char = 'a'
+
+
+//    Can be Combined
+    val s1: String = int + string
+}
