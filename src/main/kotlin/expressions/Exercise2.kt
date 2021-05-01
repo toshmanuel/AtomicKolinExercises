1d610c4c611a6c779d54a944190d4c3c3fa3d955
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-05-01 12:08:41 +0100

    Expressions Exercise 2

---
+++ src/main/kotlin/expressions/Exercise2.kt
@@ -0,0 +1,18 @@
+package expressions
+
+fun f(first: Int, second: Int): Int =
+    first + second
+
+fun g(first: String, second: String): String = "$first $second"
+
+fun h() =println("h()")
+
+fun main() {
+    val a = f(2, 3)
+    val b = g("Boy", "girl")
+    val c = h()
+
+    println("Int")
+    println("String")
+    println("Unit")
+}
