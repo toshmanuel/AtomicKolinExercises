6dd20803086e1bd13a175f7270177ad5c87fce28
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-05-01 12:29:49 +0100

    Objects Exercise1

--- src/main/kotlin/introduction_To_Objects/objects/Exercise1.kt
+++ src/main/kotlin/introduction_To_Objects/objects/Exercise1.kt
@@ -1,2 +1,9 @@
 package introduction_To_Objects.objects
 
+fun palindrome(value: String): String =
+    if (value.reversed()==value) "$value is a palindrome" else "$value is not a palindrome"
+
+fun main(args: Array<String>) {
+
+    println(palindrome("hannah"))
+}
