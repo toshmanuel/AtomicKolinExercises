171a167ec3d3800d3f012fdbd5184af50ce68f7e
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-05-01 21:17:53 +0100

    Packages Exercise 1

---
+++ src/main/kotlin/introduction_To_Objects/packages/Exercise1.kt
@@ -0,0 +1,8 @@
+package introduction_To_Objects.packages
+import kotlin.math.PI
+import kotlin.math.E
+fun main(args: Array<String>) {
+    println("The value of E is $E")
+    println("The value of PI is $PI")
+
+}
