07c71e3ac0f6181f37f5df1f89652278f31775f2
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-05-03 12:56:02 +0100

    Eceptions

---
+++ src/main/kotlin/introduction_To_Objects/exceptions/Excercise1.kt
@@ -0,0 +1,9 @@
+package introduction_To_Objects.exceptions
+
+fun main() {
+//    "12", "1.2", "1,2", "1.2e0", "1.2e1",
+//    "1.2e2", "1.2e3", "1.2e10", "12.3e10",
+//    "1.2e-1", "1.2e-10".
+
+    println("\"1,2\" cannot be converted to double ")
+}
