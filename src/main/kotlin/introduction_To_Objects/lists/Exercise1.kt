81f33d197f9a3b1ae421babb68be434f4a3e0d64
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-05-04 22:04:32 +0100

    Lists Exercise 1

---
+++ src/main/kotlin/introduction_To_Objects/lists/Exercise1.kt
@@ -0,0 +1,29 @@
+package introduction_To_Objects.lists
+
+fun findMax(numbers: List<Int>): Int{
+
+    var max = numbers[0]
+    for(i in 1..numbers.size){
+        if(numbers[i] >= max){
+            max = numbers[i]
+        }
+    }
+    return max
+}
+
+
+fun main() {
+
+    val numbers = ArrayList<Int>()
+    numbers.add(2)
+    numbers.add(4)
+    numbers.add(21)
+    numbers.add(22)
+    numbers.add(7)
+    numbers.add(102)
+    numbers.add(216)
+    numbers.add(222)
+
+    val max = findMax(numbers)
+    println(max)
+}
