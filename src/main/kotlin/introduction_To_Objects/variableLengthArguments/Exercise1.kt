0c4d8311ca023300c8806b2aa010f2b5273c337c
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-05-05 10:25:34 +0100

    Variable Length Arguments Exercise 1

---
+++ src/main/kotlin/introduction_To_Objects/variableLengthArguments/Exercise1.kt
@@ -0,0 +1,10 @@
+package introduction_To_Objects.variableLengthArguments
+
+
+//fun variableLength(vararg i: Array<String>, vararg j: Array<String>): Unit {
+//
+//}
+
+fun main() {
+    println("Multiple vararg-parameters are prohibited")
+}
