88ccff62008a158fd2f12817f6a94d8aba9f9b60
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-05-05 11:38:49 +0100

    Variable Length Arguments Exercise 2

--- src/main/kotlin/introduction_To_Objects/variableLengthArguments/Exercise4.kt
+++ src/main/kotlin/introduction_To_Objects/variableLengthArguments/Exercise4.kt
@@ -12,4 +12,11 @@ fun main() {
     val listOfLists = listOf(listOf("a", "b"), listOf("c", "d"))
     val result = flatten(listOfLists)
     println(result)
+
+
+    //sample code for IntRange
+    val r1 = IntRange(0, 10)
+    val r2 = IntRange(5, 7)
+    println(r1)
+    println(r2)
 }
