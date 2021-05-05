045bce77431da07edde2dfc3b9388f4bd9d87cc0
Oladimeji Omotosho <oladimejiomotosho5@gmail.com>
2021-05-05 10:15:38 +0100

    Lists Exercise 3

---
+++ src/main/kotlin/introduction_To_Objects/lists/Exercise3.kt
@@ -0,0 +1,11 @@
+package introduction_To_Objects.lists
+
+fun anagram(word: String, word1: String): Boolean {
+    return word.toList().sorted() == word1.toList().sorted()
+}
+
+fun main() {
+    println(anagram("joy", "oyj"))
+
+    println(anagram("mites", "times"))
+}
